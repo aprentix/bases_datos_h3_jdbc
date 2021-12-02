8b2a2d0a49e6235fcf4ac301366a489283cd3b5c
RachelDC00 <67373943+aprentix@users.noreply.github.com>
2021-12-02 01:02:34 +0100

    Update Hacha.java

--- src/main/java/game/model/Hacha.java
+++ src/main/java/game/model/Hacha.java
@@ -2,37 +2,20 @@ package game.model;
 
 public class Hacha {
     private String nombreHacha;
-    private int idHacha;
+    private int daño;
     private int pesoHacha;
-
-
-    public Hacha(String nombreHacha, int idHacha, int pesoHacha) {
+    public Hacha(String nombreHacha, int daño, int pesoHacha) {
         this.nombreHacha = nombreHacha;
-        this.idHacha = idHacha;
+        this.daño = daño;
         this.pesoHacha = pesoHacha;
     }
-
     public String getNombreHacha() {
         return nombreHacha;
     }
-
-    public void setNombreHacha(String nombreHacha) {
-        this.nombreHacha = nombreHacha;
+    public int getDañoHacha() {
+        return daño;
     }
-
-    public int getIdHacha() {
-        return idHacha;
-    }
-
-    public void setIdHacha(int idHacha) {
-        this.idHacha = idHacha;
-    }
-
     public int getPesoHacha() {
         return pesoHacha;
-    }
-
-    public void setPesoHacha(int pesoHacha) {
-        this.pesoHacha = pesoHacha;
     }
 }
